706c786bb85c713ec3d31b68d4c4a495d3c997c5
Shiraz <sheraz.m.e@gmail.com>
2023-11-16 08:14:46 +0200

    variables

--- vars.c
+++ vars.c
@@ -19,39 +19,3 @@ void get_var(char **command, int status)
 			printInt(getpid());
 		}
 }
-
-void handle_line_commands(char *line)
-{
-    char *token, *arg_token;
-    char *command[MAX_ARGUMENTS];
-    int i = 0;
-
-    // Split the line by semicolon (;)
-    token = strtok(line, ";");
-
-    while (token != NULL) {
-        i = 0;
-        // Split each command by space
-        arg_token = strtok(token, " ");
-        while (arg_token != NULL && i < MAX_ARGUMENTS - 1) {
-            command[i] = arg_token;
-            arg_token = strtok(NULL, " ");
-            i++;
-        }
-        command[i] = NULL;
-        
-        execute_command(command);
-
-        token = strtok(NULL, ";");
-    }
-}
-
-int multi_command (char *command)
-{
-	char *token;
-	
-	token = strtok(line, ";");
-	if (token != NULL)
-		return (1);
-	return (0);
-}
